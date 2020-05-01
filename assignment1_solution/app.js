f0fbc7e5565efe9e544815dfdd2d99e5d08b2180
Alexandre Lehaut <alex.lehaut@gmail.com>
2020-05-01 15:49:54 +1200

    Done + font color and border color change + only not empty item counts

--- assignment1_solution/app.js
+++ assignment1_solution/app.js
@@ -1,5 +1,37 @@
 (function () {
 'use strict';
 
+angular.module('LunchCheck', [])
+.controller('LunchCheckController', LunchCheckController);
+
+LunchCheckController.$inject = ['$scope'];
+function LunchCheckController($scope){
+  $scope.list = ""
+
+  $scope.Check = function() {
+    var nbOfItem = $scope.list.split(',').filter(isEmpty)
+    if (!$scope.list){
+      $scope.message = "Please enter data first";
+      $scope.style_message = {"color" : "red"};
+      $scope.style_list = {"border" : "solid red"};
+    } else if (nbOfItem.length > 3) {
+      $scope.message = "Too much!";
+      $scope.style_message = {"color" : "orange"};
+      $scope.style_list = {"border" : "solid orange"};
+    } else {
+      $scope.message = "Enjoy!";
+      $scope.style_message = {"color" : "green"};
+      $scope.style_list = {"border" : "solid green"};
+    }
+  };
+}
+
+function isEmpty (value) {
+    if (/\S/.test(value)) {
+      return true;
+    } else {
+      return false;
+    }
+}
 
 })();
