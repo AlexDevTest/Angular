bbe7fabd44d9e364b51ff6a6a45bad5f5f8a8484
Alexandre Lehaut <alex.lehaut@gmail.com>
2020-05-18 14:04:32 +1200

    Module 3 assignment commit of code starter

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 (function () {
 'use strict';
 
-angular.module('MsgApp', [])
-.controller('MsgController', MsgController);
+var shoppingList1 = [
+  "Milk", "Donuts", "Cookies", "Chocolate", "Peanut Butter", "Pepto Bismol", "Pepto Bismol (Chocolate flavor)", "Pepto Bismol (Cookie flavor)"
+];
 
-MsgController.$inject = ['$scope'];
-function MsgController($scope) {
-  $scope.name = "Yaakov";
-
-  $scope.sayMessage = function() {
-    return "Yaakov likes to eat healthy snacks at night!";
+var shoppingList2 = [
+  {
+    name: "Milk",
+    quantity: "2"
+  },
+  {
+    name: "Donuts",
+    quantity: "200"
+  },
+  {
+    name: "Cookies",
+    quantity: "300"
+  },
+  {
+    name: "Chocolate",
+    quantity: "5"
   }
+];
+
+angular.module('ShoppingListApp', [])
+.controller('ShoppingListController', ShoppingListController);
+
+ShoppingListController.$inject = ['$scope'];
+function ShoppingListController($scope) {
+  $scope.shoppingList1 = shoppingList1;
+  $scope.shoppingList2 = shoppingList2;
+
+  $scope.addToList = function () {
+    var newItem = {
+      name: $scope.newItemName,
+      quantity: $scope.newItemQuantity
+    };
+
+    $scope.shoppingList2.push(newItem);
+  };
 }
 
 })();
